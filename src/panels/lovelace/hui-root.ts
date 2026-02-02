e78817a2a0bd6998491d97efd1b608e2294d54fb
Paul Bottein <paul.bottein@gmail.com>
2026-02-02 17:07:10 +0100

    Fix "Reload resources" menu for YAML resource mode (#29346)

--- src/panels/lovelace/hui-root.ts
+++ src/panels/lovelace/hui-root.ts
@@ -51,7 +51,6 @@ import "../../components/ha-tab-group";
 import "../../components/ha-tab-group-tab";
 import "../../components/ha-tooltip";
 import { createAreaRegistryEntry } from "../../data/area/area_registry";
-import type { LovelacePanelConfig } from "../../data/lovelace";
 import type {
   LovelaceConfig,
   LovelaceRawConfig,
@@ -63,6 +62,7 @@ import {
   fetchDashboards,
   updateDashboard,
 } from "../../data/lovelace/dashboard";
+import { fetchLovelaceInfo } from "../../data/lovelace/resource";
 import { getPanelTitle } from "../../data/panel";
 import { createPerson } from "../../data/person";
 import { showListItemsDialog } from "../../dialogs/dialog-list-items/show-list-items-dialog";
@@ -151,6 +151,8 @@ class HUIRoot extends LitElement {
 
   @state() private _curView?: number | "hass-unused-entities";
 
+  @state() private _resourceMode: "yaml" | "storage" = "storage";
+
   private _configChangedByUndo = false;
 
   private _viewCache?: Record<string, HUIView>;
@@ -186,6 +188,7 @@ class HUIRoot extends LitElement {
 
   private _renderActionItems(): TemplateResult {
     const result: TemplateResult[] = [];
+
     if (this._editMode) {
       result.push(
         html`<ha-icon-button
@@ -231,6 +234,8 @@ class HUIRoot extends LitElement {
           </a>`
       );
     }
+
+    const isLovelaceDashboard = this.panel?.component_name === "lovelace";
 
     const items: ActionItem[] = [
       {
@@ -336,8 +341,8 @@ class HUIRoot extends LitElement {
         overflowAction: this._handleReloadResources,
         visible:
           !this._editMode &&
-          (this.hass.panels.lovelace?.config as LovelacePanelConfig)?.mode ===
-            "yaml",
+          this._resourceMode === "yaml" &&
+          isLovelaceDashboard,
         overflow: true,
       },
       {
@@ -666,6 +671,9 @@ class HUIRoot extends LitElement {
       passive: true,
     });
     this._handleUrlChanged();
+    fetchLovelaceInfo(this.hass).then((info) => {
+      this._resourceMode = info.resource_mode;
+    });
   }
 
   public connectedCallback(): void {

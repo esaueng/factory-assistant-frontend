f41188c594a3fe3249cb89e9042092296c0ab550
Paul Bottein <paul.bottein@gmail.com>
2026-06-08 15:43:51 +0200

    Redesign the Activity (logbook) as a timeline with entity context

--- src/panels/config/devices/ha-config-device-page.ts
+++ src/panels/config/devices/ha-config-device-page.ts
@@ -1010,6 +1010,7 @@ export class HaConfigDevicePage extends LitElement {
                     .time=${this._logbookTime}
                     .entityIds=${this._entityIds(entities)}
                     .deviceIds=${this._deviceIdInList(this.deviceId)}
+                    .scope=${"device"}
                     virtualize
                     narrow
                     no-icon
